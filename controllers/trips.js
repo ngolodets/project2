0d1faf7942601aee240ce60c790c804eae4ca526
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-07-01 15:38:56 -0700

    added GET /trips route to display all trips

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-
+// GET /trips - show ALL the trips that exist
+router.get('/', function(req, res) {
+  db.trip.findAll().then(function(trips) {
+    console.log("trips: " + trips);
+    res.render('trips/index', {trips});
+  });
+});
 
 
 module.exports = router;
