0e2a6eb9fc27f61c4e12d6bc47887bff4a7a9c40
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-07-01 15:16:45 -0700

    added POST /parks route and form

--- controllers/parks.js
+++ controllers/parks.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const axios = require('axios');
+const methodOverride = require('method-override');
 
 const headers = {
   'X-Api-Key': process.env.API_KEY,
@@ -13,7 +14,7 @@ router.get('/', function(req, res) {
   axios.get('https://developer.nps.gov/api/v1/parks?', {headers})
     .then(function(response) {
       var parks = response.data;
-      console.log("🐳🐳🐳 response from API: " + parks.data[1].fullName);
+      //console.log("🐳🐳🐳 response from API: " + parks.data[1].fullName);
       //res.json(parks)
       res.render('parks/index', {parks: parks.data});
   }).catch( function(err) {
@@ -21,28 +22,60 @@ router.get('/', function(req, res) {
   });
 });
 
+// POST /parks - add park to the favorites list
+router.post('/', function(req, res) {
+  db.park.create({
+    name: req.body.name,
+    state: req.body.state,
+    coordinates: req.body.coordinates,
+    code: req.body.code
+  }).then(function(data) {
+    console.log("🌸DATA: " + data);
+    res.redirect('/parks');
+  })
+})
+// router.post('/', function(req, res) {
+//   db.park.create({
+//     name: req.body.name,
+//     state: req.body.state,
+//     coordinates: req.body.coordinates,
+//     code: req.body.code
+//     }).then((park) => {
+//       db.trip.findOrCreate({
+//         where: {
+//           name: req.body.trip
+//         }
+//       }).spread(function(category, created) {
+//         park.addTrip(trip)
+//           .then(function(){
+//             console.log("🐠🐠🐠done adding " + req.body.trip);
+//             res.redirect('/');
+//         })
+//       })
+//     }).catch((error) => {
+//       res.json(error);
+//     })
+//   });
 
-//GET /parks/:id - renders show page with details of the park
+
+//GET /parks/:id - renders show page with selected parks
 router.get('/:id', function(req, res) {
   db.park.findOne({
     where: {id: parseInt(req.params.id)},
-    include: [db.trip] //, db.comment, db.tag] 
+    //include: [db.trip]  
   }).then(function(data) {
-    console.log("🌴🌴🌴 The park code is: " + data.code);
-    return axios.get('https://developer.nps.gov/api/v0/parks?parkCode=' + data.code, 
-      {headers: config})
+    console.log("🌴🌴🌴 The park code is: " + req.params.code);
+    return axios.get('https://developer.nps.gov/api/v1/parks?parkCode=' + req.params.code, 
+      {headers})
   }).then(function(response) {
-      res.render('parks/show', {park: response.data});
+      var park = response.data;
+      res.render('parks/show', {park: park.data});
   }).catch(function(error) {
       console.log(error);
   })
 });
 
   
-
-
-
-
 
 
 
